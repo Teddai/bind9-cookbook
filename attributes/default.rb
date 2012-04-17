cbfbc499e86e4fc0659b415eff6bc7c8069c9d4d
Guilhem Lettron <guilhem@lettron.fr>
2012-04-17 17:24:01 +0200

    change attribute management

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,5 @@
-default[:bind9][:enable_ipv6] = true
+# change to false because true listen on "any" 
+default[:bind9][:enable_ipv6] = false
 
 # Allow all clients to query the nameserver, no recursion
 default[:bind9][:allow_query] = nil
@@ -8,9 +9,14 @@ default[:bind9][:allow_recursion] = "none"
 default[:bind9][:allow_transfer] = "none"
 default[:bind9][:allow_update] = nil
 
-default[:bind9][:enable_forwarding] = false
-default[:bind9][:forwarders] = [ "8.8.8.8", "8.8.4.4" ]
-  
+# set a forwarders to enable forwarding
+default[:bind9][:forwarders] = nil
+#default[:bind9][:forwarders] = [ "8.8.8.8", "8.8.4.4" ]
+
+# by default listen on "any" ip address
+default[:bind9][:listen_on] = [ "any" ]
+#default[:bind9][:listen_on] = [ "key", "192.168.0.0/24" ]
+
 case node[:platform]
 when "centos", "redhat", "suse", "fedora"
   default[:bind9][:package] = "bind"
